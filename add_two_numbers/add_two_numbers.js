79396ad1703322bda805e4dcd3fa0ef9924681d1
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-07-29 16:43:44 +0100

    Part 1 MVP

--- add_two_numbers/add_two_numbers.js
+++ add_two_numbers/add_two_numbers.js
@@ -11,5 +11,20 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    
-};
+    const root = new ListNode()
+    let current = root
+    let carry = null
+    while (l1 || l2) {
+      current.next = l1 || l2 // when not to do this? we know that one of em is around so, its ok?
+      current = current.next
+      const sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + (carry || 0)
+      current.val = sum % 10
+      carry = sum >= 10 ? Math.floor(sum / 10) : null
+      l1 = l1 && l1.next
+      l2 = l2 && l2.next
+    }
+    if (carry !== null) {
+      current.next = new ListNode(carry)
+    }
+    return root.next
+  };
